f3be544c688808f18ff53b40cf098c88750f8425
Christopher Pole <chris@chrispole.com>
2018-04-07 15:22:20 +0100

    Add the ability to convert tiles into a game state

--- src/index.js
+++ src/index.js
@@ -1,9 +1,42 @@
 class Gravnic {
   /**
    * Create a Gravnic game
-   * @param {options}
+   * @param {Array} tiles - A 2D array of tiles to populate the game state from
+   * @returns {Array} A 2D array that represents the current game state
+   * @constructor
    */
-  constructor(options) {}
+  loadTiles(tiles) {
+    const gameState = [];
+    const gridSize = Math.sqrt(tiles.length);
+    let gridRow;
+
+    for (let i = 0; i < gridSize; i++) {
+      gridRow = [];
+
+      for (let j = 0; j < gridSize; j++) {
+        gridRow.push(tiles[i * gridSize + j].selectedTileId);
+      }
+
+      gameState.push(gridRow);
+    }
+
+    this.gameState = gameState;
+
+    return this.gameState;
+  }
+
+  /**
+   * Returns the current game state
+   * @returns {Array} A 2D array that represents the current game state
+   */
+  getGameState() {
+    return this.gameState;
+  }
 }
 
-export default Gravnic;
+module.exports = Gravnic;
+
+Gravnic.MOVE_UP = 'MOVE_UP';
+Gravnic.MOVE_RIGHT = 'MOVE_RIGHT';
+Gravnic.MOVE_UDOWN = 'MOVE_DOWN';
+Gravnic.MOVE_ULEFT = 'MOVE_LEFT';
--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,98 @@
 import Gravnic from './index';
 
-describe('Gravnic', () => {
-  it('Should initalize without exploding', () => {
-    const gravnic = new Gravnic();
+const testTiles = [
+  {
+    position: 1,
+    selectedTileId: 0,
+  },
+  {
+    position: 2,
+    selectedTileId: 2,
+  },
+  {
+    position: 3,
+    selectedTileId: 0,
+  },
+  {
+    position: 4,
+    selectedTileId: 0,
+  },
+  {
+    position: 5,
+    selectedTileId: 0,
+  },
+  {
+    position: 6,
+    selectedTileId: 0,
+  },
+  {
+    position: 7,
+    selectedTileId: 0,
+  },
+  {
+    position: 8,
+    selectedTileId: 3,
+  },
+  {
+    position: 9,
+    selectedTileId: 0,
+  },
+  {
+    position: 10,
+    selectedTileId: 0,
+  },
+  {
+    position: 11,
+    selectedTileId: 0,
+  },
+  {
+    position: 12,
+    selectedTileId: 0,
+  },
+  {
+    position: 13,
+    selectedTileId: 4,
+  },
+  {
+    position: 14,
+    selectedTileId: 0,
+  },
+  {
+    position: 15,
+    selectedTileId: 0,
+  },
+  {
+    position: 16,
+    selectedTileId: 1,
+  },
+];
 
-    expect(gravnic instanceof Gravnic).toBe(true);
+const testGameState = [[0, 2, 0, 0], [0, 0, 0, 3], [0, 0, 0, 0], [4, 0, 0, 1]];
+
+describe('Gravnic', () => {
+  describe('The constructor', () => {
+    it('Should initalize without exploding', () => {
+      const gravnic = new Gravnic();
+
+      expect(gravnic instanceof Gravnic).toBe(true);
+    });
+  });
+
+  describe('loadTiles()', () => {
+    it('Converts the tiles into a starting game state', () => {
+      const gravnic = new Gravnic();
+      const gameState = gravnic.loadTiles(testTiles);
+
+      expect(gameState).toEqual(testGameState);
+    });
+  });
+
+  describe('getGameState()', () => {
+    it('Should return the current game state', () => {
+      const gravnic = new Gravnic();
+      gravnic.loadTiles(testTiles);
+
+      expect(gravnic.getGameState()).toEqual(testGameState);
+    });
   });
 });
